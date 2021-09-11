729a6d4a85104b46be7818db6aea3a5f6f9f1568
Argha Nilanjon Nondi <pcic095@gmail.com>
2021-09-11 15:41:37 +0600

    project completed

---
+++ js/script.js
@@ -0,0 +1,44 @@
+let amount = document.getElementById("calculator-input-amount");
+let rate = document.getElementById("calculator-input-rate");
+let year = document.getElementById("calculator-input-year");
+let result_box = document.getElementById("calculator-result");
+let submit = document.getElementById("container-submit");
+let show_rate = document.getElementById("calculator-interest-display");
+
+function showRate() {
+  show_rate.innerHTML = `${rate.value}%`;
+  hideResult();
+}
+
+function showResult() {
+  result_box.style.display = "block";
+}
+
+function hideResult() {
+  result_box.style.display = "none";
+}
+
+hideResult();
+
+show_rate.innerHTML = `${rate.value}%`;
+
+function compute() {
+  showResult();
+  result_box.innerHTML = `<p>
+            If you deposit <span class="calculator-highlight">${
+              amount.value
+            }</span><br />
+            at an interest rate of
+            <span class="calculator-highlight">${rate.value}%</span>.<br />
+            You will receive an amount of
+            <span class="calculator-highlight">${
+              amount.value * rate.value * year.value
+            }</span>,<br />
+            in the year <span class="calculator-highlight">${year.value}</span>
+          </p>`;
+}
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  compute();
+});
